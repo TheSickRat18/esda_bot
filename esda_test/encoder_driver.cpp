ff004faca49b7d8d4e39a9e5ab5a33ef5d45d3f3
TheSickRat18 <166204389+TheSickRat18@users.noreply.github.com>
2025-12-17 10:30:08 +1100

    add encoder polling

--- esda_test/encoder_driver.cpp
+++ esda_test/encoder_driver.cpp
@@ -15,16 +15,16 @@
 #define A3144_ENC_COUNTER
 
 #if defined(A3144_ENC_COUNTER)
-
-/* ================= STATE ================= */
+/*
+// STATE
 volatile long left_enc_pos  = 0L;
 volatile long right_enc_pos = 0L;
 
-/* ================= ISR DECLS ================= */
+// ISR
 void leftEncoderISR();
 void rightEncoderISR();
 
-/* ================= INIT ================= */
+// INIT
 void initEncoders() {
   pinMode(LEFT_ENC_PIN, INPUT_PULLUP);
   pinMode(RIGHT_ENC_PIN, INPUT_PULLUP);
@@ -36,7 +36,7 @@ void initEncoders() {
   Serial.println("Encoders initialsed");
 }
 
-/* ================= ISR ================= */
+// ISR
 void leftEncoderISR() {
   //Serial.println("Triggered");
   left_enc_pos++;
@@ -47,7 +47,7 @@ void rightEncoderISR() {
   right_enc_pos++;
 }
 
-/* ================= API ================= */
+
 long readEncoder(int motor_idx) {
   
   if (motor_idx == LEFT)
@@ -73,3 +73,65 @@ void resetEncoders() {
   #error "No encoder driver selected"
 #endif
 
+*/
+
+/* ================= STATE ================= */
+volatile long left_enc_pos  = 0L;
+volatile long right_enc_pos = 0L;
+
+static uint8_t left_last_state  = HIGH;
+static uint8_t right_last_state = HIGH;
+
+/* ================= INIT ================= */
+void initEncoders() {
+  pinMode(LEFT_ENC_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_ENC_PIN, INPUT_PULLUP);
+
+  left_last_state  = digitalRead(LEFT_ENC_PIN);
+  right_last_state = digitalRead(RIGHT_ENC_PIN);
+
+  Serial.println("Encoders initialised (polling)");
+}
+
+/* ================= POLLING UPDATE ================= */
+/* Call this as fast as possible (e.g. every loop()) */
+void updateEncoders() {
+  uint8_t left_state  = digitalRead(LEFT_ENC_PIN);
+  uint8_t right_state = digitalRead(RIGHT_ENC_PIN);
+
+  // Detect falling edge (HIGH -> LOW)
+  if (left_last_state == HIGH && left_state == LOW) {
+    left_enc_pos++;
+  }
+
+  if (right_last_state == HIGH && right_state == LOW) {
+    right_enc_pos++;
+  }
+
+  left_last_state  = left_state;
+  right_last_state = right_state;
+}
+
+/* ================= API ================= */
+long readEncoder(int motor_idx) {
+  if (motor_idx == LEFT)
+    return left_enc_pos;
+  else
+    return right_enc_pos;
+}
+
+void resetEncoder(int motor_idx) {
+  if (motor_idx == LEFT)
+    left_enc_pos = 0L;
+  else
+    right_enc_pos = 0L;
+}
+
+void resetEncoders() {
+  resetEncoder(LEFT);
+  resetEncoder(RIGHT);
+}
+
+#else
+  #error "No encoder driver selected"
+#endif
